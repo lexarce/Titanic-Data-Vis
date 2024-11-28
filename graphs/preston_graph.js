be35aa37da0881557162d854c0e4323b24f7102f
Preston <101310062+colorfulparadox@users.noreply.github.com>
2024-11-28 12:15:57 -0700

    oopsie

--- graphs/preston_graph.js
+++ graphs/preston_graph.js
@@ -3,9 +3,9 @@ document.addEventListener('DOMContentLoaded', function () {
     console.log("preston graph");
 
     Promise.all([d3.csv('data/Titanic-Dataset.csv')])
-         .then(function (values) {
-             
-         });
+    .then(function (values) {
+        
+    });
 
 
 });
